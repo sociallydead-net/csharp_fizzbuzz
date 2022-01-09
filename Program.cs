54586f21037eb61cc80fe5013d7329c6900eaa9f
Andreas Fischer <84879821+sociallydead-net@users.noreply.github.com>
2022-01-09 17:24:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * FizzBuzz
+ * Copyright (C) 2022 Andreas Fischer / Sociallydead.net
+ * 
+ * The sociallydead.net version of FizzBuss...
+ * A reply to an awesome video by Tom Scott (I am a big fan!)
+ * You can see his video and this javascript version here:
+ * https://www.youtube.com/watch?v=QPZ0pIK_wsc
+ * 
+ * Meanwhile leave it to me to complety overengineer a childs game :)
+ * My solution seperates the evaluation and the rendering, it also
+ * works on a "ruleSet" so you can change the number and text in a 
+ * central place, you can also easily at a 3rd or 4th value etc.
+ * 
+ * Also I HATE else statements because they can become messy really fast,
+ * so if you wonder why I dont use else in the render part that is why!
+ * 
+ * I hope you like my solution, and if it helps you at a job interview you
+ * can buy me a beer over paypal: afischer@jsyntax.com
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+class FizzBuss
+{
+    private int from;
+    private int to;
+    private string format;
+    private Dictionary<int, string> ruleSet;
+
+    public FizzBuss(int from, int to, Dictionary<int, string> ruleSet)
+    {
+        this.from = from;
+        this.to = to;
+        this.ruleSet = ruleSet;
+
+        int length = to.ToString().Length;
+        format =  "D"+length.ToString();
+    }
+
+
+    public void Run()
+    {
+        for (int i=from; i<=to; i++)
+        {
+            Dictionary<int, string> resultSet = Evaluate(i);
+            Render(i, resultSet);
+        }
+    }
+
+    protected Dictionary<int, string> Evaluate(int value)
+    {
+        Dictionary<int, string> resultSet = new Dictionary<int, string>();
+        foreach (KeyValuePair<int, string> entry in ruleSet)
+        {
+            if (value % entry.Key == 0)
+            {
+                resultSet.Add(entry.Key, entry.Value);
+            }
+        }
+        return resultSet;
+    }
+
+    protected void Render(int value, Dictionary<int, string> resultSet)
+    {
+        ConsoleColor consoleColor = Console.ForegroundColor;
+
+        int results = resultSet.Count;
+
+        if (results == 0)
+            Console.ForegroundColor = ConsoleColor.White;
+        if (results != 0)
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+        Console.Write(value.ToString(format));
+        Console.Write(": ");
+
+        
+        if (results>=2)
+            Console.ForegroundColor = ConsoleColor.Red;
+        if (results==1)
+            Console.ForegroundColor = ConsoleColor.Green;
+
+        foreach (KeyValuePair<int, string> result in resultSet)
+        {
+            Console.Write(result.Value);
+        }
+
+        Console.Write(Environment.NewLine);
+
+        Console.ForegroundColor = consoleColor;
+    }
+
+    public static void Main()
+    {
+        Dictionary<int, string> ruleSet = new Dictionary<int, string>();
+        ruleSet.Add(4, "Fizz");
+        ruleSet.Add(5, "Buzz");
+        ruleSet.Add(7, "Sociallydead");
+
+        FizzBuss fizzBuss = new FizzBuss(1, 100, ruleSet); 
+        fizzBuss.Run();
+    }
+}
+
